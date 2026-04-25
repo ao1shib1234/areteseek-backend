b8cd9bd7bb0da127153c280262983ed1e401da87
ao1shib1234 <172578024@qq.com>
2026-04-25 15:41:29 +0800

    fix: allow Vercel and custom domains in CORS for enquiry submissions

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,18 @@
+const DEFAULT_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://areteseek-frontend.vercel.app',
+  'https://yuy-precision.com',
+  'https://www.yuy-precision.com',
+];
+
+const extraOrigins = (process.env.CORS_ORIGINS || '')
+  .split(',')
+  .map((s) => s.trim())
+  .filter(Boolean);
+
+const allowedOrigins = [...new Set([...DEFAULT_ORIGINS, ...extraOrigins])];
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
@@ -7,8 +22,14 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:3000', 'http://localhost:3001']
-    }
+      origin: (ctx) => {
+        const requestOrigin = ctx.request.header.origin;
+        if (requestOrigin && /^https:\/\/areteseek-frontend-[a-z0-9-]+\.vercel\.app$/.test(requestOrigin)) {
+          return [...allowedOrigins, requestOrigin];
+        }
+        return allowedOrigins;
+      },
+    },
   },
   'strapi::poweredBy',
   'strapi::query',
